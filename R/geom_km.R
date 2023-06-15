7d433bc988ceac8ed8a348e68cdc026b006553ac
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2023-06-15 11:57:17 +0200

    start with km geoms

---
+++ R/geom_km.R
@@ -0,0 +1,76 @@
+#' Add a Kaplan-Meier Survival Curve
+#'
+#' @description `r lifecycle::badge("experimental")`
+#' Adds the Kaplan-Meier survival curve.
+#'
+#' @inheritParams ggplot2::geom_point
+#'
+#' @section Aesthetics:
+#' `geom_km()` understands the following aesthetics (required aesthetics in bold):
+#'
+#' - **`x`**: the survival/censoring times, automatically mapped by [stat_km()].
+#' - **`y`**: the survival probability estimates, automatically mapped by [stat_km()].
+#' - `alpha`
+#' - `color`
+#' - `linetype`
+#' - `linewidth`
+#'
+#' @seealso The default `stat` for this `geom` is [stat_km()], see
+#'   there for more options to control the underlying statistical
+#'   transformation.
+#'
+#' @author Samer Mouksassi
+#' @export
+#' @examples
+#' library(ggplot2)
+#' sex <- rbinom(250, 1, .5)
+#' df <- data.frame(
+#'   time = exp(rnorm(250, mean = sex)),
+#'   status = rbinom(250, 1, .75),
+#'   sex = sex
+#' )
+#' ggplot(df, aes(time = time, status = status, color = factor(sex))) + geom_km()
+geom_km <- function(mapping = NULL,
+                    data = NULL,
+                    stat = "km",
+                    position = "identity",
+                    show.legend = NA,
+                    inherit.aes = TRUE,
+                    na.rm = TRUE,
+                    ...) {
+  ggplot2::layer(
+    geom = GeomKm,
+    mapping = mapping,
+    data = data,
+    stat = stat,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+#' @rdname ggplot2.utils-ggproto
+#' @format NULL
+#' @usage NULL
+#' @author Samer Mouksassi
+#' @keywords internal
+#' @export
+GeomKm <- ggplot2::ggproto(
+  "GeomKm",
+  ggplot2::Geom,
+  draw_group = function(data, scales, coordinates, ...) {
+    path <- transform(data, alpha = NA)
+    ggplot2::GeomPath$draw_panel(path, scales, coordinates)
+  },
+  required_aes = c("x", "y"),
+  default_aes = ggplot2::aes(
+    colour="black",
+    fill="grey60",
+    linewidth=.75,
+    linetype=1,
+    weight=1,
+    alpha = 1
+  ),
+  draw_key = ggplot2::draw_key_path
+)
---
+++ R/geom_km_ticks.R
@@ -0,0 +1,86 @@
+#' Add Tick Marks to a Kaplan-Meier Survival Curve
+#'
+#' @description `r lifecycle::badge("experimental")`
+#' Adds tickmarks at the times when there are censored observations but no
+#' events.
+#'
+#' @inheritParams ggplot2::geom_point
+#'
+#' @seealso The default `stat` for this `geom` is [stat_km_ticks()], see
+#'   there for more options to control the underlying statistical
+#'   transformation.
+#'
+#' @author Samer Mouksassi
+#' @export
+#' @examples
+#' library(ggplot2)
+#' sex <- rbinom(250, 1, .5)
+#' df <- data.frame(
+#'   time = exp(rnorm(250, mean = sex)),
+#'   status = rbinom(250, 1, .75),
+#'   sex = sex
+#' )
+#' ggplot(df, aes(time = time, status = status, color = factor(sex), group = factor(sex))) +
+#'  geom_km() +
+#'  geom_kmticks(col = "black")
+geom_km_ticks <- function(mapping = NULL,
+                          data = NULL,
+                          stat = "km_ticks",
+                          position = "identity",
+                          show.legend = NA,
+                          inherit.aes = TRUE,
+                          na.rm = TRUE,
+                          ...) {
+  ggplot2::layer(
+    geom = GeomKmTicks,
+    mapping = mapping,
+    data = data,
+    stat = stat,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+# GeomKmTicks ----
+
+#' @rdname ggplot2.utils-ggproto
+#' @format NULL
+#' @usage NULL
+#' @keywords internal
+#' @export
+GeomKmTicks <- ggplot2::ggproto(
+  "GeomKmTicks",
+  ggplot2::Geom,
+  draw_group = function(data, scales, coordinates, ...) {
+    showpoints <- data$n.censor > 0 & data$n.event == 0
+    coordsp <- coordinates$transform(data, scales)[showpoints, , drop = FALSE]
+    if (nrow(coordsp) == 0) {
+      grid::nullGrob()
+    } else {
+      grid::pointsGrob(
+        coordsp$x,
+        coordsp$y,
+        pch = coordsp$shape,
+        size = grid::unit(coordsp$size, "char"),
+        gp = grid::gpar(
+          col = coordsp$colour,
+          fill = coordsp$fill,
+          alpha = coordsp$alpha
+        )
+      )
+    }
+  },
+  required_aes = c("x", "y"),
+  non_missing_aes = c("size", "shape"),
+  default_aes = ggplot2::aes(
+    shape = 3,
+    colour = "black",
+    size = .75,
+    alpha = 1,
+    stroke = 0.5,
+    fill = "black"
+  ),
+  draw_key = ggplot2::draw_key_point
+)
